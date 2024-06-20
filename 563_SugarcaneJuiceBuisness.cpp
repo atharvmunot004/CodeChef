353654f493c4505acff25404aad79af3d1ac73fc
atharvmunot004 <munot.atharv@gmail.com>
2024-06-20 09:31:30 +0530

    Create 563_SugarcaneJuiceBuisness.cpp

---
+++ 563_SugarcaneJuiceBuisness.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans[n];
+    
+    for (int i = 0; i < n; i ++) {
+        int n;
+        cin >> n;
+        ans[i] = n * 15;
+    }
+    
+    for (int i = 0; i < n; i ++) {
+        cout << ans[i] << endl;
+    }
+}
