808b95dacab745b35214dde001380e3ff886071d
atharvmunot004 <munot.atharv@gmail.com>
2024-06-20 08:45:11 +0530

    Create 561_ExpertSetter.cpp

---
+++ 561_ExpertSetter.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int ans[n];
+    
+    for (int i = 0; i < n; i++) {
+        float x, y;
+        cin >> x >> y;
+        
+        if ((y / x) >= 0.5) {
+            ans[i] = 1;
+        }
+        else {
+            ans[i] = 0;
+        }
+    }
+    
+    for (int i = 0; i < n; i ++) {
+        if (ans[i]) {
+            cout << "YES" << endl;
+        }
+        else {
+            cout << "NO" << endl;
+        }
+    }
+	// your code goes here
+
+}
