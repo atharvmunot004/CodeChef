2e51767e7e62c42cf8b8bda09e526d92a31cae8f
atharvmunot004 <munot.atharv@gmail.com>
2024-06-20 17:45:19 +0530

    Create 613_JengaNight.cpp

---
+++ 613_JengaNight.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<string> ans(n);
+    
+    for (int i = 0; i < n; i ++) {
+        int n, x;
+        cin >> n >> x;
+        
+        if (x % n == 0) {
+            ans[i] = "YES";
+        }
+        else {
+            ans[i] = "NO";
+        }
+    }
+    
+    for (const auto& res : ans) {
+        cout << res << endl;
+    }
+    
+    return 0;
+}
