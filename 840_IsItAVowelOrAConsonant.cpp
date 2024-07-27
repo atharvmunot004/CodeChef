f580728ac99a6c7fe3b1862412c8188461c2a068
atharvmunot004 <munot.atharv@gmail.com>
2024-07-27 23:47:25 +0530

    Create 840_IsItAVowelOrAConsonant.cpp

---
+++ 840_IsItAVowelOrAConsonant.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') {
+        cout << "Vowel" << endl;
+    } else {
+        cout << "Consonant" << endl;
+    }
+    return 0;
+	// your code goes here
+
+}
