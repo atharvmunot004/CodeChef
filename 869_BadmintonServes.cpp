18f11de71651a5129d465761d1babafe0afc220a
atharvmunot004 <munot.atharv@gmail.com>
2024-07-28 00:30:20 +0530

    Create 869_BadmintonServes.cpp

---
+++ 869_BadmintonServes.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        long long P;
+        cin >> P;
+        cout << (P / 2) + 1 << endl;
+    }
+    return 0;
+	// your code goes here
+
+}
