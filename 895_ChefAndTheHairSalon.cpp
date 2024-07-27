b8a7e1ee0d430d6a0e174eb9df37259c92772c15
atharvmunot004 <munot.atharv@gmail.com>
2024-07-28 02:30:11 +0530

    Create 895_ChefAndTheHairSalon.cpp

---
+++ 895_ChefAndTheHairSalon.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;  // Read the number of test cases
+    
+    while (T--) {
+        int N, M;
+        cin >> N >> M;  // Read N and M for each test case
+        cout << N * M << endl;  // Calculate and output the waiting time
+    }
+    
+    return 0;
+	// your code goes here
+
+}
