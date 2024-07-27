2297825166c8d55cebe94cae68d221d1ed4fd2ff
atharvmunot004 <munot.atharv@gmail.com>
2024-07-28 02:01:18 +0530

    Create 883_FarmersLeague.cpp

---
+++ 883_FarmersLeague.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    for (int i = 0; i < t; i ++) {
+        int64_t n;
+        cin >> n;
+        
+        int64_t diff = 3 * (n / 2);
+        cout << diff << endl;
+    }
+    
+    return 0;
+	// your code goes here
+
+}
