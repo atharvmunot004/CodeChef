9a7f93486b5d2b1cc77f90ae3fac4f130e5be790
atharvmunot004 <munot.atharv@gmail.com>
2024-07-28 10:27:25 +0530

    Create 933_BackToCampus.cpp

---
+++ 933_BackToCampus.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        float n, k;
+        cin >> n >> k;
+        
+        cout << ceil(n / k) << endl;
+    }
+    
+    return 0;
+	// your code goes here
+
+}
