f794e7e6baf33028592a9b47656cf67def10762c
atharvmunot004 <munot.atharv@gmail.com>
2024-07-29 08:24:47 +0530

    Update 1254_ChefAndTheWildcardMatching.cpp

--- 1254_ChefAndTheWildcardMatching.cpp
+++ 1254_ChefAndTheWildcardMatching.cpp
@@ -1,1 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string X, Y;
+        cin >> X >> Y;
+
+        bool canMatch = true;
+        
+        for (int i = 0; i < X.length(); ++i) {
+            if (X[i] != Y[i] && X[i] != '?' && Y[i] != '?') {
+                canMatch = false;
+                break;
+            }
+        }
+
+        if (canMatch) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+    
+    return 0;
+}
 
